012ff18e230376afd94810e344ddaad751997714
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2021-07-20 15:46:25 +0800

    Ex. 3-8

--- SQL/w3/query.sql
+++ SQL/w3/query.sql
@@ -13,3 +13,46 @@ SELECT * FROM salesman;
 
 SELECT 'This is SQL Exercise, Practice and Solution';
 
+/* Ex. 3.  
+   Write a query to display three numbers in three columns.
+*/
+
+SELECT 1, 2, 3;
+
+SELECT 1 AS cl1, 2 AS cl2, 3 AS cl3;
+
+/* Ex. 4.  
+   Write a query to display the sum of two numbers 10 and 15 from RDMS sever.
+*/
+
+SELECT 10 + 15;
+
+/* Ex. 5.  
+   Write a query to display the result of an arithmetic expression.
+*/
+
+SELECT (10 + 15) * 20;
+
+/* Ex. 6.  
+   Write a SQL statement to display specific columns like name and commission for all the salesmen.
+   Sample table: salesman
+*/
+
+SELECT name, commission FROM salesman;
+
+/* Ex. 7.  
+   Write a query to display the columns in a specific order like order date, salesman id, order number and purchase amount from for all the orders.
+   Sample table: orders
+*/
+
+SELECT ord_date AS "order date", salesman_id AS "salesman id", ord_no AS "order number", purch_amt AS "purchase amount" FROM orders;
+
+/* Ex. 8.  
+   Write a query which will retrieve the value of salesman id of all salesmen, getting orders from the customers in orders table without any repeats.   
+   Sample table: orders
+*/
+
+SELECT DISTINCT salesman_id FROM orders;
+
+
+
