e9132c781096b00b29b98fdd3eaa21d2bcf4d959
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2021-07-30 15:13:25 +0800

    Update query.sql
    Changed code style. Competed ex. 1-5

--- SQL/w3/query.sql
+++ SQL/w3/query.sql
@@ -1,6 +1,8 @@
 /* Connect to sqlex database
    Exercises from w3 */
-   
+
+/* PART 1. Retrieve data from tables. */
+
 /* Ex. 1.  
    Write a SQL statement to display all the information of all salesmen.
 */
@@ -60,7 +62,7 @@ SELECT DISTINCT salesman_id FROM orders;
 */
 
 SELECT name, city FROM salesman
-WHERE city = 'Paris';
+ WHERE city = 'Paris';
 
 /* Ex. 10.  
    Write a SQL statement to display all the information for those customers with a grade of 200.   
@@ -68,8 +70,8 @@ WHERE city = 'Paris';
 */
 
 SELECT * 
-FROM customer
-WHERE grade = 200;
+  FROM customer
+ WHERE grade = 200;
 
 /* Ex. 11.  
     Write a SQL query to display the order number followed by order date and the purchase amount for each order which will be delivered 
@@ -78,8 +80,8 @@ WHERE grade = 200;
 */
 
 SELECT ord_no, ord_date, purch_amt
-FROM orders
-WHERE salesman_id = 5001;
+  FROM orders
+ WHERE salesman_id = 5001;
 
 /* Ex. 12.  
     Write a SQL query to display the Nobel prizes for 1970.
@@ -87,8 +89,8 @@ WHERE salesman_id = 5001;
 */
 
 SELECT *
-FROM nobel_win
-WHERE year = 1970;
+  FROM nobel_win
+ WHERE year = 1970;
 
 /* Ex. 13.  
     Write a SQL query to know the winner of the 1971 prize for Literature.
@@ -96,9 +98,9 @@ WHERE year = 1970;
 */
 
 SELECT winner
-FROM   nobel_win
-WHERE  year = 1971
-AND    subject = 'Literature';
+  FROM nobel_win
+ WHERE year = 1971
+   AND subject = 'Literature';
 
 /* Ex. 14.  
     Write a SQL query to display the year and subject that won 'Dennis Gabor' his prize.
@@ -106,8 +108,8 @@ AND    subject = 'Literature';
 */
 
 SELECT year, subject
-FROM nobel_win
-WHERE winner = 'Dennis Gabor';
+  FROM nobel_win
+ WHERE winner = 'Dennis Gabor';
 
 /* Ex. 15.  
     Write a SQL query to give the name of the 'Physics' winners since the year 1950.   
@@ -115,9 +117,9 @@ WHERE winner = 'Dennis Gabor';
 */
 
 SELECT winner
-FROM   nobel_win
-WHERE  subject = 'Physics'
-AND    year >= 1950;
+  FROM nobel_win
+ WHERE subject = 'Physics'
+   AND year >= 1950;
 
 /* Ex. 16.  
     Write a SQL query to Show all the details (year, subject, winner, country ) of the Chemistry prize winners between the year 1965 to 1975 inclusive.
@@ -125,9 +127,9 @@ AND    year >= 1950;
 */
 
 SELECT year, subject, winner, country
-FROM   nobel_win
-WHERE  year BETWEEN 1965 AND 1975
-AND    subject = 'Chemistry';
+  FROM nobel_win
+ WHERE year BETWEEN 1965 AND 1975
+   AND subject = 'Chemistry';
 
 /* Ex. 17.  
     Write a SQL query to show all details of the Prime Ministerial winners after 1972 of Menachem Begin and Yitzhak Rabin.  
@@ -135,9 +137,9 @@ AND    subject = 'Chemistry';
 */
 
 SELECT * 
-FROM   nobel_win
-WHERE  year > 1972 
-AND    (winner = 'Menachem Begin' OR winner = 'Yitzhak Rabin');
+  FROM nobel_win
+ WHERE year > 1972 
+   AND (winner = 'Menachem Begin' OR winner = 'Yitzhak Rabin');
 
 /* Ex. 18.  
     Write a SQL query to show all the details of the winners with first name Louis.    
@@ -145,8 +147,8 @@ AND    (winner = 'Menachem Begin' OR winner = 'Yitzhak Rabin');
 */
 
 SELECT *
-FROM nobel_win
-WHERE winner LIKE 'Louis %';
+  FROM nobel_win
+ WHERE winner LIKE 'Louis %';
 
 /* Ex. 19.  
     Write a SQL query to show all the winners in Physics for 1970 together with the winner of Economics for 1971.   
@@ -154,9 +156,9 @@ WHERE winner LIKE 'Louis %';
 */
 
 SELECT winner
-FROM   nobel_win
-WHERE  (year = 1970 AND subject = 'Physics')
-OR     (year = 1971 AND subject = 'Economics');
+  FROM nobel_win
+ WHERE (year = 1970 AND subject = 'Physics')
+    OR (year = 1971 AND subject = 'Economics');
 
 /* Ex. 20.  
     Write a SQL query to show all the winners of nobel prize in the year 1970 except the subject Physiology and Economics.   
@@ -164,15 +166,15 @@ OR     (year = 1971 AND subject = 'Economics');
 */
 
 SELECT winner
-FROM   nobel_win
-WHERE  year = 1970 
-AND    subject <> 'Physiology'
-AND    subject <> 'Economics';
+  FROM nobel_win
+ WHERE year = 1970 
+  AND  subject <> 'Physiology'
+  AND  subject <> 'Economics';
 
 SELECT winner
-FROM   nobel_win
-WHERE  year = 1970 
-AND    subject NOT IN ('Physiology', 'Economics');
+  FROM nobel_win
+ WHERE year = 1970 
+   AND subject NOT IN ('Physiology', 'Economics');
 
 /* Ex. 21.  
     Write a SQL query to show the winners of a 'Physiology' prize in an early year before 1971 together with winners of a 'Peace' prize 
@@ -181,16 +183,16 @@ AND    subject NOT IN ('Physiology', 'Economics');
 */
 
 SELECT winner
-FROM   nobel_win
-WHERE  (year < 1971 AND subject = 'Physiology')
-OR     (year > 1974 AND subject = 'Peace');
+  FROM nobel_win
+ WHERE (year < 1971 AND subject = 'Physiology')
+    OR (year > 1974 AND subject = 'Peace');
 
 SELECT winner
-FROM   nobel_win
-WHERE  (year < 1971 AND subject = 'Physiology')
-UNION  (SELECT winner 
-		FROM   nobel_win 
-		WHERE  (year > 1974 AND subject = 'Peace'));
+  FROM nobel_win
+ WHERE (year < 1971 AND subject = 'Physiology')
+ UNION (SELECT winner 
+		  FROM nobel_win 
+		 WHERE (year > 1974 AND subject = 'Peace'));
 
 /* Ex. 22.  
    Write a SQL query to find all details of the prize won by Johannes Georg Bednorz. 
@@ -198,8 +200,8 @@ UNION  (SELECT winner
 */
 
 SELECT *
-FROM   nobel_win
-WHERE  winner = 'Johannes Georg Bednorz';
+  FROM nobel_win
+ WHERE winner = 'Johannes Georg Bednorz';
 
 /* Ex. 23.  
    Write a SQL query to find all the details of the nobel winners for the subject not started with the letter 'P' and 
@@ -207,10 +209,10 @@ WHERE  winner = 'Johannes Georg Bednorz';
    Sample table: nobel_win
 */
 
-SELECT   *
-FROM     nobel_win
-WHERE    subject NOT LIKE 'P%'
-ORDER BY year DESC, winner; 
+SELECT *
+  FROM nobel_win
+ WHERE subject NOT LIKE 'P%'
+ ORDER BY year DESC, winner; 
 
 /* Ex. 24.  
    Write a SQL query to find all the details of 1970 winners by the ordered to subject and winner name; 
@@ -219,31 +221,31 @@ ORDER BY year DESC, winner;
 */
 
 -- 1
-SELECT   *
-FROM     nobel_win
-WHERE    year = 1970
-ORDER BY 
+SELECT *
+  FROM nobel_win
+ WHERE year = 1970
+ ORDER BY 
 		 CASE 
-			  WHEN subject IN('Chemistry', 'Economics') THEN 1
-			  ELSE 0
+		 WHEN subject IN('Chemistry', 'Economics') THEN 1
+		 ELSE 0
 		 END ASC,
 		 subject,
 		 winner;
 		 
 -- 2
-(SELECT  *
-FROM     nobel_win
-WHERE    year = 1970
-AND      subject NOT IN ('Chemistry', 'Economics')
-ORDER BY subject, winner)
+(SELECT *
+   FROM nobel_win
+  WHERE year = 1970
+    AND subject NOT IN ('Chemistry', 'Economics')
+  ORDER BY subject, winner)
 
-UNION ALL
+  UNION ALL
 
-(SELECT   * 
-FROM      nobel_win
-WHERE     year = 1970
-AND       subject IN ('Chemistry', 'Economics')
-ORDER BY  subject ASC, winner);
+(SELECT * 
+   FROM nobel_win
+  WHERE year = 1970
+    AND subject IN ('Chemistry', 'Economics')
+  ORDER BY  subject ASC, winner);
 
 /* Ex. 25.  
    Write a SQL query to find all the products with a price between Rs.200 and Rs.600.    
@@ -251,8 +253,8 @@ ORDER BY  subject ASC, winner);
 */
 
 SELECT pro_name 
-FROM item_mast
-WHERE pro_price BETWEEN 200 AND 600;
+  FROM item_mast
+ WHERE pro_price BETWEEN 200 AND 600;
 
 /* Ex. 26.  
    Write a SQL query to calculate the average price of all products of the manufacturer which code is 16.    
@@ -260,8 +262,8 @@ WHERE pro_price BETWEEN 200 AND 600;
 */
 
 SELECT ROUND(AVG(pro_price), 2) AS avg_price
-FROM   item_mast
-WHERE  pro_com = 16;
+  FROM item_mast
+ WHERE pro_com = 16;
 
 /* Ex. 27.  
    Write a SQL query to find the item name and price in Rs.    
@@ -269,7 +271,7 @@ WHERE  pro_com = 16;
 */
 
 SELECT pro_name AS "Item name", pro_price AS "Price, Rs"
-FROM item_mast;
+  FROM item_mast;
 
 /* Ex. 28.  
    Write a SQL query to display the name and price of all the items with a price is equal or more than Rs.250, 
@@ -277,19 +279,19 @@ FROM item_mast;
    Sample table: item_mast
 */
 
-SELECT   pro_name AS "Name", pro_price AS "Price"
-FROM     item_mast
-WHERE    pro_price >= 250
-ORDER BY pro_price DESC, pro_name; 
+SELECT pro_name AS "Name", pro_price AS "Price"
+  FROM item_mast
+ WHERE pro_price >= 250
+ ORDER BY pro_price DESC, pro_name; 
 
 /* Ex. 29.  
    Write a SQL query to display the average price of the items for each company, showing only the company code.  
    Sample table: item_mast
 */
 
-SELECT   pro_com, ROUND(AVG(pro_price), 2) 
-FROM     item_mast
-GROUP BY pro_com;
+SELECT pro_com, ROUND(AVG(pro_price), 2) 
+  FROM item_mast
+ GROUP BY pro_com;
 
 /* Ex. 30.  
    Write a SQL query to find the name and price of the cheapest item(s).  
@@ -297,8 +299,8 @@ GROUP BY pro_com;
 */
 
 SELECT pro_name, pro_price
-FROM   item_mast
-WHERE  pro_price = (SELECT MIN(pro_price) FROM item_mast);
+  FROM item_mast
+ WHERE pro_price = (SELECT MIN(pro_price) FROM item_mast);
 
 /* Ex. 31.  
    Write a query in SQL to find the last name of all employees, without duplicates.   
@@ -306,7 +308,7 @@ WHERE  pro_price = (SELECT MIN(pro_price) FROM item_mast);
 */
 
 SELECT DISTINCT emp_lname 
-FROM emp_details;
+  FROM emp_details;
 
 /* Ex. 32.  
    Write a query in SQL to find the data of employees whose last name is 'Snares'.  
@@ -314,19 +316,91 @@ FROM emp_details;
 */
 
 SELECT *
-FROM emp_details 
-WHERE  emp_lname = 'Snares';
+  FROM emp_details 
+ WHERE emp_lname = 'Snares';
 
 /* Ex. 33.  
-Write a query in SQL to display all the data of employees that work in the department 57.   
-Sample table: emp_details
+   Write a query in SQL to display all the data of employees that work in the department 57.   
+   Sample table: emp_details
 */
 
 SELECT * 
-FROM emp_details
-WHERE emp_dept = 57;
+  FROM emp_details
+ WHERE emp_dept = 57;
 
 
+/* PART 2. Using Boolean and Relational operators. */
 
+/* Ex. 1.  
+   Write a query to display all customers with a grade above 100.   
+   Sample table: customer
+*/
 
+SELECT cust_name AS "Customer", grade AS "Grade"
+  FROM customer
+ WHERE grade > 100;
 
+/* Ex. 2.  
+   Write a query statement to display all customers in New York who have a grade value above 100.    
+   Sample table: customer
+*/
+
+SELECT cust_name AS "Customer",
+       city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE city = 'New York'
+   AND grade > 100;
+
+/* Ex. 3.  
+   Write a SQL statement to display all customers, who are either belongs to the city New York or had a grade above 100.    
+   Sample table: customer
+*/
+
+SELECT cust_name AS "Customer",
+	   city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE city = 'New York'
+    OR grade > 100;
+
+/* Ex. 4.  
+   Write a SQL statement to display all the customers, who are either belongs to the city New York or not had a grade above 100.    
+   Sample table: customer
+*/
+
+-- 1
+SELECT cust_name AS "Customer",
+       city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE city = 'New York'
+    OR grade <= 100;
+	
+-- 2
+SELECT cust_name AS "Customer",
+       city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE city = 'New York'
+    OR NOT grade > 100;
+ 
+/* Ex. 5.  
+   Write a SQL query to display those customers who are neither belongs to the city New York nor grade value is more than 100.    
+   Sample table: customer
+*/
+
+-- 1
+SELECT cust_name AS "Customer",
+       city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE city <> 'New York'
+   AND grade <= 100;
+
+-- 2 
+SELECT cust_name AS "Customer",
+       city AS "City",
+	   grade AS "Grade"
+  FROM customer
+ WHERE NOT (city = 'New York' OR grade > 100);
