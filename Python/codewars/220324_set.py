4702a4a719907f4fb53110132d26210855946e88
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2022-04-04 19:39:29 +0800

    Codewars

---
+++ Python/codewars/220324_set.py
@@ -0,0 +1,14 @@
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new 
+# sorted string, the longest possible, containing distinct letters - each 
+# taken only once - coming from s1 or s2.
+
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(a1, a2):
+    return ''.join(sorted(list(set(a1 + a2))))
