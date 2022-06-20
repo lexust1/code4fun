46ebc6603be42aeb231274e8c02c973df74bdfdc
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2022-06-20 20:21:02 +0800

    cl

---
+++ Pandas/w3/data_frames/002.py
@@ -0,0 +1,24 @@
+# Write a Pandas program to create and display a DataFrame from a specified 
+# dictionary data which has the index labels.
+
+# Sample DataFrame:
+    
+# exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+# 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+# 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+# 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+# labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+import pandas as pd
+import numpy as np
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 
+                      'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'], 
+             'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+             'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1], 
+             'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 
+                         'no', 'no', 'yes']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=exam_data, index=labels)
