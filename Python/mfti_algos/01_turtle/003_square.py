dd57bb81b84d31e9d104c616bb4fefa286c94ac9
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2023-01-11 12:16:56 +0800

    Add the 2nd method for square.

--- Python/mfti_algos/01_turtle/003_square.py
+++ Python/mfti_algos/01_turtle/003_square.py
@@ -3,6 +3,7 @@ import turtle
 
 
 def main():
+    # The 1st method.
     turtle.shape("turtle")
     turtle.forward(50)
     turtle.left(90)
@@ -11,7 +12,13 @@ def main():
     turtle.forward(50)
     turtle.left(90)
     turtle.forward(50)
-
+    # The 2nd method.
+    turtle.penup()
+    turtle.goto(100, 200)
+    turtle.pendown()
+    for part in range(4):
+        turtle.forward(50)
+        turtle.left(90)
 
 if __name__ == "__main__":
     main()
