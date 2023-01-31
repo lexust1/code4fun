906877cbafa119b319cfbd22910c7694b58ed4d8
Aleksey Ustinov <43818738+lexust1@users.noreply.github.com>
2023-01-31 13:03:13 +0800

    Change Ex.9

--- Python/mfti_algos/02_robot/task_9.py
+++ Python/mfti_algos/02_robot/task_9.py
@@ -8,7 +8,7 @@ def task_8_2():
     while not wall_is_on_the_right():
         if wall_is_beneath() and wall_is_above():
             move_right()
-        if wall_is_beneath() and not wall_is_above():
+        elif wall_is_beneath() and not wall_is_above():
             fill_cell()
             move_right()
     if wall_is_on_the_right() and not wall_is_above():
